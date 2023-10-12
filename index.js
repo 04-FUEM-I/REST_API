40e71ed4f3f0c1a4396e8c190315dacb29b89c51
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-10-12 06:48:24 +0000

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import
+
+const app = require('express')();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+    PORT,
+    () => console.log(`server ready at http://localhost"${PORT}`)
+);
